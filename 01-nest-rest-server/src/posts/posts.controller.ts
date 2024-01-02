5bd94be67827d0cc836cc484e9ae7f9df04b1c8f
Beren <berenickt99@gmail.com>
2024-01-02 17:32:21 +0900

    ADD:커스텀 데코레이터의 data 파라미터 사용

--- 01-nest-rest-server/src/posts/posts.controller.ts
+++ 01-nest-rest-server/src/posts/posts.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Request, UseGuards } from '@nestjs/common'
 import { PostsService } from './posts.service'
 import { AccessTokenGuard } from 'src/auth/guard/bearer-token.guard'
+import { User } from 'src/users/decorator/user.decorator'
+import { UsersModel } from 'src/users/entities/users.entity'
 
 @Controller('posts')
 export class PostsController {
@@ -30,14 +32,13 @@ export class PostsController {
   @Post()
   @UseGuards(AccessTokenGuard)
   postPosts(
-    @Request() req: any,
+    @User('id') userId: number,
     @Body('title') title: string, //
     @Body('content') content: string,
     // 기본값을 true로 설정하는 파이프
     // @Body('isPublic', new DefaultValuePipe(true)) isPublic: boolean,
   ) {
-    const authorId = req.user.id
-    return this.postsService.createPost(authorId, title, content)
+    return this.postsService.createPost(userId, title, content)
   }
 
   /*** 4) PATCH /posts/:id
