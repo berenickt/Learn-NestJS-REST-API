d58010bd86cf1a49716e4ebbbb5b96b92632648a
Beren <berenickt99@gmail.com>
2024-01-02 22:49:32 +0900

    ADD:Exclude Annotation

--- 01-nest-rest-server/src/users/users.controller.ts
+++ 01-nest-rest-server/src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common'
+import { ClassSerializerInterceptor, Controller, Get, UseInterceptors } from '@nestjs/common'
 import { UsersService } from './users.service'
 
 @Controller('users')
@@ -6,6 +6,14 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
+  /*** ClassSerializerInterceptor 뜻
+   * Serialization(직렬화)
+   * - 현재 시스템에서 사용되는 (NestJS) 데이터 구조를 다른 시스템에서도 사용할 수 있는 포맷으로 변환
+   * - class의 object에서 JSON 포맷으로 변환
+   *
+   * Deserialization(역직렬화) : 직렬화의 반대
+   */
+  @UseInterceptors(ClassSerializerInterceptor)
   getUsers() {
     return this.usersService.getAllUsers()
   }
