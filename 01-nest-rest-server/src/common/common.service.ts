0205d774e16af789bce67778350b64e67e8e651c
Beren <berenickt99@gmail.com>
2024-01-04 06:09:22 +0900

    ADD:ParseWhereFilter

--- 01-nest-rest-server/src/common/common.service.ts
+++ 01-nest-rest-server/src/common/common.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common'
+import { BadRequestException, Injectable } from '@nestjs/common'
 import { FindManyOptions, FindOptionsOrder, FindOptionsWhere, Repository } from 'typeorm'
 import { BasePaginationDto } from './dto/base-pagination.dto'
 import { BaseModel } from './entities/base.entity'
+import { FILTER_MAPPER } from './const/filter-mapper.const'
 
 @Injectable()
 export class CommonService {
@@ -86,10 +87,69 @@ export class CommonService {
     }
   }
 
+  /*** 길이가 3일 경우
+   * e.g. where__id__more_than을 __를 기준으로 나누면,
+   * ['where', 'id', 'more_than']으로 나눌 수 있다.
+   */
   private parseWhereFilter<T extends BaseModel>(
     key: string, //
     value: any,
-  ): FindOptionsWhere<T> {}
+  ): FindOptionsWhere<T> {
+    const options: FindOptionsWhere<T> = {}
+    const split = key.split('__')
+
+    if (split.length !== 2 && split.length !== 3) {
+      throw new BadRequestException(
+        `where 필터는 '__'로 split 햇을 떄, 길이가 2 또는 3이어야 합니다 - 문제되는 키값: ${key}`,
+      )
+    }
+
+    /*** 길이가 2일 경우 where__id = 3을
+     * FindOptionsWhere로 풀어보면 아래와 같다
+     * {
+     * 	where: {
+     * 		id : 3,
+     * 	}
+     * }
+     */
+    if (split.length === 2) {
+      // ['where', 'id']
+      const [_, field] = split
+      // field -> 'id, value -> 3
+      options[field] = value
+    } else {
+      /*** 길이가 3일 경우 Typeorm 유틸리티 적용이 필요한 경우다
+       * where__id__more_than의 경우
+       * where는 버려도 되고, 두번쨰 값은 필터할 키값이 되고,
+       * 세번쨰 값은 typeorm 유틸리티가 된다.
+       *
+       * FILTER_MAPPER에 미리 정의해둔 값들로
+       * field 값에 FILTER_MAPPER에 해당되는 utility를 가져온 후
+       * 값에 적용해준다.
+       */
+      // ['where', 'id', 'more_than']
+      const [_, field, operator] = split
+
+      /*** where__id__between = 3, 4
+       * 만약 split 대상 문자가 존재하지 않으면, 길이가 무조건 1이다.
+       */
+      // const values = value.toString().split(',')
+
+      /***
+       * field -> id
+       * operator -> more_than
+       * FILTER_MAPPER[operator] -> MoreThan
+       */
+      // if (operator === ' between') {
+      //   options[field] = FILTER_MAPPER[operator](values[0], values[1])
+      // } else {
+      //   options[field] = FILTER_MAPPER[operator](value)
+      // }
+      options[field] = FILTER_MAPPER[operator](value)
+    }
+
+    return options
+  }
 
   private parseOrderFilter<T extends BaseModel>(
     key: string, //
