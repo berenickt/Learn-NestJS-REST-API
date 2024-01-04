85021cdc83c5f9bc9ff25828869e930e2208067f
Beren <berenickt99@gmail.com>
2024-01-05 03:06:09 +0900

    ADD:Static File Serving 옵션

--- 01-nest-rest-server/src/app.module.ts
+++ 01-nest-rest-server/src/app.module.ts
@@ -18,10 +18,20 @@ import {
   ENV_DB_PORT_KEY,
   ENV_DB_USERNAME_KEY,
 } from './common/const/env-keys.const'
+import { ServeStaticModule } from '@nestjs/serve-static'
+import { PUBLIC_FOLDER_PATH } from './common/const/path.const'
 
 @Module({
   imports: [
     PostsModule,
+    /***
+     * http://localhost:3000/public/posts/4022.jpg
+     * http://localhost:3000/posts/4022.jpg
+     */
+    ServeStaticModule.forRoot({
+      rootPath: PUBLIC_FOLDER_PATH,
+      serveRoot: '/public',
+    }),
     ConfigModule.forRoot({
       envFilePath: '.env',
       isGlobal: true,
