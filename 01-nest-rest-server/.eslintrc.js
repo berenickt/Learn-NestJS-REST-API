f7589da0f8346eae3a73724d80906f70ab67561f
Beren <berenickt99@gmail.com>
2024-01-04 18:23:19 +0900

    ADD:no-unused-vars-eslint

--- 01-nest-rest-server/.eslintrc.js
+++ 01-nest-rest-server/.eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
   ignorePatterns: ['.eslintrc.js'],
   rules: {
     'prettier/prettier': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
