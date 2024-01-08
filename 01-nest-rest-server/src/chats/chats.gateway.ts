fe4010f11d93bbaf0d16891dea4f4067f0a85816
Beren <berenickt99@gmail.com>
2024-01-08 17:19:50 +0900

    ADD:Room 활용하기

--- 01-nest-rest-server/src/chats/chats.gateway.ts
+++ 01-nest-rest-server/src/chats/chats.gateway.ts
@@ -20,9 +20,22 @@ export class ChatsGateway implements OnGatewayConnection {
     console.log(`on connect called : ${socket.id}`)
   }
 
+  @SubscribeMessage('enter_chat')
+  enterChat(
+    @MessageBody() data: number[], //
+    @ConnectedSocket() socket: Socket,
+  ) {
+    for (const chatId of data) {
+      socket.join(chatId.toString())
+    }
+  }
+
   // socket.on('send_message', (message)=>{ console.log(message)})
   @SubscribeMessage('send_message')
-  sendMessage(@MessageBody() message: string) {
-    this.server.emit('receive_message', 'hello from server')
+  sendMessage(@MessageBody() message: { message: string; chatId: number }) {
+    // 방에 들어간 사용자에게만 메시지 보내기
+    this.server
+      .in(message.chatId.toString()) //
+      .emit('receive_message', message.message)
   }
 }
